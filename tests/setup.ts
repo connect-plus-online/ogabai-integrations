ab1bce5fcc39a773da73a96520819ede9f1dedd9
Agu Chijioke <chokey2nv@gmail.com>
2026-07-24 15:42:26 +0100

    afterAll test

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,8 +5,15 @@ beforeAll(async () => {
   
 });
 
-afterAll(() => {
+afterAll(async () => {
   console.log("All tests done.");
+  try {
+      const r = await fetch("http://localhost:8080/health");
+      console.log("Health:", r.status);
+  } catch {
+      console.log("Backend is dead.");
+  }
+  
 });
 
 
